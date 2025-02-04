d9b68b1abd693e7da60c3a204021b10ef61c9607
Jonathan Javier Jaguaco Quituisaca <116780208+JonathanJQ03@users.noreply.github.com>
2025-02-04 16:56:47 -0500

    Add files via upload

---
+++ CapaEntidad/TipoMedicamentoCLS.cs
@@ -0,0 +1,10 @@
+﻿namespace CapaEntidad
+{
+    public class TipoMedicamentoCLS
+    {
+        //primera parte de mi capa: creacion de entidad es decir lo mismo que un constructor mediante propiedad
+        public int idTipoMedicamento { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+    }
+}
