b588b175f63ff9c7080a7caf92353f94ffa88cde
Jonathan Javier Jaguaco Quituisaca <116780208+JonathanJQ03@users.noreply.github.com>
2025-02-04 16:56:26 -0500

    Add files via upload

---
+++ CapaDatos/TipoMedicamentoDAL.cs
@@ -0,0 +1,26 @@
+﻿using CapaEntidad;
+
+namespace CapaDatos
+{
+    public class TipoMedicamentoDAL
+    {
+        public List<TipoMedicamentoCLS> ListartipoMedicamento()
+        {
+            List<TipoMedicamentoCLS> lista = new List<TipoMedicamentoCLS>();
+            lista.Add(new TipoMedicamentoCLS
+            {
+                idTipoMedicamento = 1,
+                nombre = "Pastillas",
+                descripcion = "Medicamento en forma de pastillas"
+            });
+            lista.Add(new TipoMedicamentoCLS
+            {
+                idTipoMedicamento = 2,
+                nombre = "Jarabe",
+                descripcion = "Medicamento en forma de jarabe"
+            });
+
+            return lista;
+        }
+    }
+}
